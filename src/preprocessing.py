f6ea46e373868b236b2c45543561c55480dd66fc
auerxaver <auer.xaver@googlemail.com>
2023-05-20 00:30:28 +0200

    implement watershedding and contour analysis, works quite well on some examples, but not that consistent

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -7,16 +7,77 @@ import numpy as np
 class Preprocessing:
     def __init__(self, im_paths):
         self.im_paths = im_paths
-        self.imgs = [cv2.imread(im_path, 0) for im_path in im_paths]
+        self.imgs = [cv2.imread(im_path) for im_path in im_paths]
         self.incision = []
         self.stitches = []
         for img in self.imgs:
-            img_proc = self.thresholding_plus(img)
-            self.incision.append(self.find_incision(img_proc))
-            #self.stitches.append()
-            self.visualize(img_proc)
+            img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            thresh = self.thresholding_plus(img_gray)
+            watershed, markers = self.test_watershed(thresh, img)
+            contours = self.test_contours(thresh)
+            #self.incision.append(self.find_incision(img_proc))
+            self.visualize(watershed)
+            self.visualize(contours)
+            #self.visualize(markers)
+
+            # visualization using the markers from watershedding to draw contours (kinda good!)
+            self.visualize(self.test_contours(markers))
+
+
+    # This seems to seperate the areas quite well, not sure if it's useful yet though. Might be worth looking into more
+    def test_watershed(self, inverse, img_color):
+        # noise removal
+        kernel = np.ones((3,3),np.uint8)
+        opening = cv2.morphologyEx(inverse,cv2.MORPH_OPEN,kernel, iterations = 2)
+
+        # sure background area
+        sure_bg = cv2.dilate(opening,kernel,iterations=4)
+
+        # Finding sure foreground area
+        dist_transform = cv2.distanceTransform(opening,cv2.DIST_L2,5)
+        ret, sure_fg = cv2.threshold(dist_transform,0.2*dist_transform.max(),255,0)
+
+        # Finding unknown region
+        sure_fg = np.uint8(sure_fg)
+        unknown = cv2.subtract(sure_bg,sure_fg)
+
+
+        # Marker labelling
+        ret, markers = cv2.connectedComponents(sure_fg)
+        # Add one to all labels so that sure background is not 0, but 1
+        markers = markers+1
+        # Now, mark the region of unknown with zero
+        markers[unknown==255] = 0
+        markers_thresh = 255*(markers > 1).astype(np.uint8)
+
+        markers_watershed = cv2.watershed(img_color, markers)
+        img_color[markers_watershed == -1] = [255,0,0]
+
+        return img_color, markers_thresh
+
+
+
+    # Locates the incisions and stitches somewhat well, couple of errors though, not sure if they're fixed easily
+    # Maybe combine with watershedding?
+    # e.g watershedding to create markers, and use those as reference for the contours, would get rid of unnecessary noise
+    # Maybe there's an easier way to 
+    def test_contours(self, inverse):
+        contours, _ = cv2.findContours(inverse, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+        filtered_contours = []
+        for contour in contours:
+            area = cv2.contourArea(contour)
+            if area > 0 and area < 100000000:
+                filtered_contours.append(contour)
+
+        inverse_new = cv2.merge([inverse,inverse, inverse])
+        for contour in filtered_contours:
+            x, y, w, h = cv2.boundingRect(contour)
+            cv2.rectangle(inverse_new, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+        return inverse_new
 
     def thresholding_plus(self, img):
+        # test
         # Apply Gaussian blur for noise reduction
         blurred = cv2.GaussianBlur(img, (5, 5), 0)
 
@@ -25,7 +86,6 @@ class Preprocessing:
 
         # Reverse black/white values
         inverse = np.invert(thresholding)       
-
         return inverse
 
     # function for extracting horizontal lines from image - not useful yet
@@ -55,14 +115,12 @@ class Preprocessing:
         stitches = [] 
         return stitches
 
-    def find_stitches(self, img):
-        return 0
 
 
 
     def visualize(self, img):
-        x = [self.incision[0][0][0], self.incision[0][1][0]]
-        y = [self.incision[0][0][1], self.incision[0][1][1]]
-        plt.plot(x, y, 'r')
-        plt.imshow(img, cmap="gray")
+        #x = [self.incision[0][0][0], self.incision[0][1][0]]
+        #y = [self.incision[0][0][1], self.incision[0][1][1]]
+        #plt.plot(x, y, 'r')
+        plt.imshow(img)
         plt.show()
